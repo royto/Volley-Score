d2fb30b3b17909e2f87ff944ffc2df91dbc923eb
Julien <royjulien@gmail.com>
2014-03-09 19:03:19 +0100

    use of locale service for pluralization

--- app/scripts/controllers/history.js
+++ app/scripts/controllers/history.js
@@ -1,13 +1,22 @@
 angular.module('volleyApp')
-    .controller('HistoryCtrl', ['$scope', 'gamesService', function ($scope, gamesService) {
+    .controller('HistoryCtrl', ['$scope', '$locale', 'gamesService', function ($scope, $locale, gamesService) {
 
         'use strict';
 
+        //Pluralize nb games saved
+        $scope.gameForms = {
+            0: 'aucun match sauvegardé',
+            one: '{} match sauvegardé',
+            other: '{} matchs sauvegardés'
+        };
+
         $scope.games = gamesService.getSavedGames();
+
         $scope.removeGame = function (index) {
             gamesService.removeSavedMatch(index);
             $scope.games = gamesService.getSavedGames();
         };
+
         $scope.clearGames = function () {
             gamesService.clearSavedMatch();
             $scope.games = [];
