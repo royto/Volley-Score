d22b8c37983fee2525f2651aaa7f8d0f75b27f8c
Julien <royjulien@gmail.com>
2014-05-04 18:50:39 +0200

    add isSetOver unitTest

--- test/spec/services/matchservice.js
+++ test/spec/services/matchservice.js
@@ -15,4 +15,65 @@ describe('Service: MatchService', function () {
     expect(!!MatchService).toBe(true);
   });
 
+  describe('isSetOver', function () {
+    var match;
+
+    describe('normal Set', function () {
+
+       beforeEach(function () {
+        match = MatchService.getMatch();
+        match.currentSet = 1;
+      });
+
+      it('should return set over', function () {
+        match.scoreTeam1[0] = 25;
+        match.scoreTeam2[0] = 13;
+
+        expect(MatchService.isSetOver()).toBeTruthy();
+      });
+
+      it('should return set not over', function () {
+        match.scoreTeam1[0] = 15;
+        match.scoreTeam2[0] = 14;
+
+        expect(MatchService.isSetOver()).toBeFalsy();
+      });
+
+      it('should return set not over', function () {
+        match.scoreTeam1[0] = 25;
+        match.scoreTeam2[0] = 24;
+
+        expect(MatchService.isSetOver()).toBeFalsy();
+      });
+    });
+
+    describe('5th Set', function () {
+
+      beforeEach(function () {
+        match = MatchService.getMatch();
+        match.currentSet = 5;
+      });
+
+      it('should return set not over', function () {
+        match.scoreTeam1[4] = 12;
+        match.scoreTeam2[4] = 14;
+
+        expect(MatchService.isSetOver()).toBeFalsy();
+      });
+
+      it('should return set over', function () {
+        match.scoreTeam1[4] = 15;
+        match.scoreTeam2[4] = 13;
+
+        expect(MatchService.isSetOver()).toBeTruthy();
+      });
+
+      it('should return set not over', function () {
+        match.scoreTeam1[4] = 15;
+        match.scoreTeam2[4] = 14;
+
+        expect(MatchService.isSetOver()).toBeFalsy();
+      });
+    });
+  });
 });
