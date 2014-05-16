da8ca228fa19136971dc61ee5f64454ec8afb221
royto <royto81+github@gmail.com>
2014-05-16 22:35:56 +0200

    fix issue with form validation

--- app/scripts/controllers/match.js
+++ app/scripts/controllers/match.js
@@ -8,7 +8,7 @@ angular.module('volleyApp')
 
       //public functions
       $scope.startGame = function () {
-        if ($startMatchForm.$valid) {
+        if ($scope.startMatchForm.$valid) {
           matchService.startGame();
         }
       };
